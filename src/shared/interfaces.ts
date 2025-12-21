b43e5bad53c03d4254c051b14470fe282de0f2aa
BrNi05 <szocsbarnabas8@gmail.com>
2025-12-22 00:33:41 +0100

    DEV: add username field to ICommandQueueItem

--- src/shared/interfaces.ts
+++ src/shared/interfaces.ts
@@ -1,14 +1,28 @@
 export interface ICommandQueueItem {
+  // Discord user ID
   user: string;
+
+  // Guild-specific username
+  // Only matters when show as part of a response to a user commands (only cosmetic)
+  // Internal commands and many admin ones have this assigned as 'root' or '', but is not important
+  username: string;
+
+  // Command to execute
   cmd: string;
 }
 
 export interface IPCResponse {
+  // Response flag
   valid: boolean;
 }
 
 export interface IFileWritePayload {
+  // File URL (Discord CDN)
   url: string;
+
+  // Destination path on server
   path: string;
+
+  // Command queue item
   payload: ICommandQueueItem;
 }
